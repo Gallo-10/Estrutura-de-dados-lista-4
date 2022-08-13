11af98a70e248675885f469ebabacbd50bbf2b1c
Gabriel Marques Gallo <80481877+Gallo-10@users.noreply.github.com>
2022-08-12 22:06:07 -0300

    Add files via upload

---
+++ Fila/Main.java
@@ -0,0 +1,22 @@
+public class Fila {
+
+    public static void main(String[] args) {
+        ListaDinamica lista = new ListaDinamica();
+
+           Atleta atleta1=new Atleta("Gabriel",1.77f,70.7f,"Capoeira","Swift");
+           Atleta atleta2=new Atleta("Gafa",1.80f,54.8f,"Ciclismo","Guarana");
+         
+
+           lista.insere(new No(atleta1));
+           lista.insere(new No(atleta2));
+           
+           lista.imprime();
+           
+           lista.remove();
+           
+           System.out.println("");
+           System.out.println("Fila apos remocao:");
+           
+           lista.imprime();
+    }
+    }
